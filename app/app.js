a081f44967919e902b0272caab56661c1749985d
Enric Corberó <ecorbero@gmail.com>
2022-06-29 11:11:26 +0200

    api-rest

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+const routerUser = require('./routes/routeUser');
+
+app.use('/user', routerUser )
+
+app.listen( 8000, function(){
+    console.log("server is running")
+    })
---
+++ app/routes/routeUser.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const routerUser = express();
+
+routerUser.get('/', function(req,res){
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl; 
+  res.json({ name: 'Andrea', age: '46', url: fullUrl })
+})
+
+module.exports = routerUser
